0dd772c963e59f0ca06647bd5a2e923a632a7686
Chirapat <chirapat.ch@ku.th>
2024-03-12 23:02:02 +0700

    test_circle

--- test_circle.py
+++ test_circle.py
@@ -7,6 +7,37 @@ Write unit tests as described in README.md.
 3. Unit test that circle constructor raises exception of radius is negative.
 
 """
+import unittest
+from math import hypot, pi
 from circle import Circle
 
-# TODO write 3 tests as described above
+class TestCircle(unittest.TestCase):
+
+    def setUp(self):
+        self.c1 = Circle(3) # typical value
+        self.c2 = Circle(4) # typical value
+        self.c3 = Circle(0) # edge case
+        self.c4 = None
+
+    def test_add_area_typical(self):
+        circle = self.c1.add_area(self.c2)
+        radius1 = self.c1.get_radius()
+        radius2 = self.c2.get_radius()
+        final_radius = hypot(radius1, radius2)
+        area = pi * final_radius ** 2
+
+        self.assertAlmostEqual(circle.get_area(), area, places=5)
+
+    def test_add_area_edge_case(self):
+        circle = self.c1.add_area(self.c3)
+        radius1 = self.c1.get_radius()
+        radius2 = self.c3.get_radius()
+        final_radius = hypot(radius1, radius2)
+        area = pi * final_radius ** 2
+
+        self.assertAlmostEqual(circle.get_area(), area, places=5)
+
+    def test_negative_radius(self):
+        with self.assertRaises(ValueError):
+            self.c4 = Circle(-1)
+
